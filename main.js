fd353a3f5b41af859727a0bed04717813643bcc4
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2020-10-23 09:49:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function TurnOff(){
+  document.getElementById("img1").src    ="https://base.imgix.net/files/base/ebm/tdworld/image/2019/11/tdworld_20573_lightbulb_getty.png?auto=format&fit=crop&h=432&w=768";
+}
+
+function TurnOn(){
+  document.getElementById("img1").src="https://images.pexels.com/photos/577514/pexels-photo-577514.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260";
+}
